5f425ad892e2714cc7c8f83c73102f475a10d3b8
Taylor Niles <tniles320@gmail.com>
2020-11-17 14:13:32 -0800

    adding page to add companies to live database

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -92,6 +92,12 @@ module.exports = function(app) {
     }
   });
 
+  app.get("/add-company", (req, res) => {
+    if(req.user) {
+      res.sendFile(path.join(__dirname, "../public/add-company.html"));
+    }
+  });
+
   // route to a specific client and there comments
   app.get("/clients/:id", (req, res) => {
     if (req.user) {
