3b97784e35213de12f5fbfdfe86126921b52e6fa
Jackie Perez <67940416+jcquper@users.noreply.github.com>
2020-11-18 14:54:49 -0800

    hope this works

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
@@ -12,19 +11,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
 $(document).ready(function(){
   $('.sidenav').sidenav();
-=======
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems);
-});
-
-// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-// var collapsibleElem = document.querySelector('.collapsible');
-// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-// Or with jQuery
-
-$(document).ready(function(){
-  $('.sidenav').sidenav();
->>>>>>> 233c7807e8f3b72fb8d6f08a9cbcf82b017d4559
 });
